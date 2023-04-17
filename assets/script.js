ae6e6b7bba4fdb9c8cf39049e4d7357c5123fdf0
Roman Parfentev <romanntev@gmail.com>
2023-04-17 14:59:13 +0200

    upload project

---
+++ assets/script.js
@@ -0,0 +1,431 @@
+function toggle1() {
+    var x = document.getElementById("setup");
+
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    }
+
+    else {
+        x.style.display = "block";
+    }
+
+    var x = document.getElementById("änderungen");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("branch");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("prüfen");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("publish");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("pfade");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("intro");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+}
+
+function toggle2() {
+    var x = document.getElementById("setup");
+
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("änderungen");
+    
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    }
+    
+    else {
+        x.style.display = "block";
+    }
+
+    var x = document.getElementById("branch");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("prüfen");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("publish");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("pfade");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("intro");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+}
+
+function toggle3() {
+    var x = document.getElementById("setup");
+
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("änderungen");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("branch");
+    
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    }
+    
+    else {
+        x.style.display = "block";
+    }
+
+    var x = document.getElementById("prüfen");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("publish");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("pfade");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("intro");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+}
+
+function toggle4() {
+    var x = document.getElementById("setup");
+
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("änderungen");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("branch");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("prüfen");
+    
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    }
+    
+    else {
+        x.style.display = "block";
+    }
+
+    var x = document.getElementById("publish");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("pfade");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("intro");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+}
+
+function toggle5() {
+    var x = document.getElementById("setup");
+
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("änderungen");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("branch");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("prüfen");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("publish");
+    
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    }
+    
+    else {
+        x.style.display = "block";
+    }
+
+    var x = document.getElementById("pfade");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("intro");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+}
+
+function toggle6() {
+    var x = document.getElementById("setup");
+
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("änderungen");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("branch");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("prüfen");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("publish");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+
+    var x = document.getElementById("pfade");
+    
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    }
+    
+    else {
+        x.style.display = "block";
+    }
+
+    var x = document.getElementById("intro");
+    
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    }
+    
+    else {
+        x.style.display = "none";
+    }
+}
